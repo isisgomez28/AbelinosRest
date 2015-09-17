af05acd33344b073d903d6082c646208b2f4e06c
isisgomez28 <igomezp.28@gmail.com>
2015-09-17 11:27:49 -0400

    _

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,7 +1,7 @@
 
 // call the packages we need
-var express    = require('express');                        // call express
-var app       = module.exports = express();                 // define our app using express
+var express	= require('express');                        // call express
+var app		= module.exports = express();                 // define our app using express
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 app.get('/api', function(req, res) {
