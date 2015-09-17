7fbe13945a2fe01b69a2d52835ed9386abb85e02
isisgomez28 <igomezp.28@gmail.com>
2015-09-17 11:08:52 -0400

    Creacion del Modelo USER

---
+++ models/user.js
@@ -0,0 +1,24 @@
+var orm = require ("orm");
+
+orm.connect("my sql://admin:Pass123@hots/AbelinosRestaurant", function (err, db) {
+	// Captura de error conexión con base de datos
+	if (err) 
+		throw err;
+
+	// Propiedades del Modelo
+	var User = db.define("person", {
+		username		: String,
+		name			: String,
+		lastname		: String,
+		email			: String,
+		password		: Password,
+		isAdmin			: Boolean,
+		passwordReset	: String
+	}, {
+		methods: {
+			fullname: function () {
+				return this.name + '' + this.lastname;
+			}
+		}
+	});
+});
