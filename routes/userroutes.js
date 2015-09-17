d3b109eaa06eda0bd8d42a9927ba1f8c13c0954a
isisgomez28 <igomezp.28@gmail.com>
2015-09-17 16:51:28 -0400

    Fixing User routing

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,9 +1,9 @@
+var express	= require('express'); 
+var router 	= express.Router();
+var i = 0;
 
-// call the packages we need
-var express	= require('express');                        // call express
-var app		= module.exports = express();                 // define our app using express
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-app.get('/api', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+router.use(function(req, res, next){
+	res.json({message: 'hooray! welcome to our api!'});
 });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,17 +4,14 @@ var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var userRoutes = require('./routes/userRoutes');
 
-//var router = express.Router();              // get an instance of the express Router
-
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080;	// set our port
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use(userRoutes);
 
 // START THE SERVER
