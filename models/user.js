129a68a0543b2cdea54239d1510a00fe5e9f34c2
isisgomez28 <igomezp.28@gmail.com>
2015-09-17 20:07:42 -0400

    working on it

--- models/user.js
+++ models/user.js
@@ -1,6 +1,13 @@
-var orm = require ("orm");
+var orm		= require ("orm");
+var mysql	= require('mysql');
 
-orm.connect("my sql://admin:Pass123@host/AbelinosRestaurant", function (err, db) {
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'admin',
+  password : 'pass123'
+});
+
+orm.connect(connection, function (err, db) {
 	// Captura de error conexión con base de datos
 	if (err) 
 		throw err;
@@ -8,7 +15,7 @@ orm.connect("my sql://admin:Pass123@host/AbelinosRestaurant", function (err, db)
 	// Propiedades del Modelo
 	var User = db.define('user', {
 		username		: {type: String, require: true, key: true},
-		name			: {type: String, require: true}
+		name			: {type: String, require: true},
 		lastname		: {type: String, require: true},
 		email			: {type: String, require: true},
 		password		: {type: Password, require: true},
@@ -19,20 +26,19 @@ orm.connect("my sql://admin:Pass123@host/AbelinosRestaurant", function (err, db)
 			fullname: function () {
 				return this.name + ' ' + this.lastname;		
 			}
-		}
-	}, {
+		}, 
 		validations: {
 			username: orm.enforce.unique("Cuenta de Usuario ya existe")
 		}
 	});
 
-	db.sync(function(err) {
-		if (err)
-			throw err;
+// 	db.sync(function(err) {
+// 		if (err)
+// 			throw err;
 
-		User.create({username: "admin", name: "Administrador", lastname: "Administrador", email: "", password: "Pass123", isAdmin: true}, function(err){
-			if (err)
-				throw err;
-		});
-	});
+// 		User.create({username: "admin", name: "Administrador", lastname: "Administrador", email: "", password: "Pass123", isAdmin: true}, function(err){
+// 			if (err)
+// 				throw err;
+// 		});
+// 	});
 });
